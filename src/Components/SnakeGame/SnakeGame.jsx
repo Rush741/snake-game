2302d96c45d2add1f7723dd5a27d8ba7361d8fd3
Rushikesh Jadhav <rajadhav147@gmail.com>
2026-01-13 22:08:30 +0530

    board ui completed

--- src/Components/SnakeGame/SnakeGame.jsx
+++ src/Components/SnakeGame/SnakeGame.jsx
@@ -1,8 +1,21 @@
 import React from 'react';
+import "./SnakeGame.css";
 
 const SnakeGame = () => {
   return (
-    <div>SnakeGame</div>
+    <div className="container">
+        <div className="boardContainer">
+            <div className="score">
+                <h1 className="currentScore">000</h1>
+                <h1 className="highScore">000</h1>
+            </div>
+            <div className="board">
+                <div className="playground">
+                    
+                </div>
+            </div>
+        </div>
+    </div>
   )
 }
 
